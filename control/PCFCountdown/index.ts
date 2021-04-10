e237b21d3f09dc94e48ee3d1e0325ec7d0f6ef45
Cathal Noonan <cathalnoonan@live.com>
2021-04-10 21:47:18 +0100

    Maintainance: Bump versions, use yarn, build scripts

--- control/PCFCountdown/index.ts
+++ control/PCFCountdown/index.ts
@@ -1,49 +1,49 @@
-import { IInputs, IOutputs } from "./generated/ManifestTypes";
-import * as countdown from 'countdown';
+import { IInputs, IOutputs } from './generated/ManifestTypes'
+import * as countdown from 'countdown'
 
 export function PCFCountdown(): ComponentFramework.StandardControl<IInputs, IOutputs> {
 
-    let _context: ComponentFramework.Context<IInputs>;
-    let _countdownDiv: HTMLDivElement;
-    let _interval: number | null;
+    let _context: ComponentFramework.Context<IInputs>
+    let _countdownDiv: HTMLDivElement
+    let _interval: number | null
 
     function start(): void {
-        _interval = window.setInterval(loop, 1000); // Every second
+        _interval = window.setInterval(loop, 1000) // Every second
     }
 
     function loop(): void {
-        const text = getDisplayText();
-        _countdownDiv.innerText = text;
-        _countdownDiv.title = text;
+        const text = getDisplayText()
+        _countdownDiv.innerText = text
+        _countdownDiv.title = text
     }
 
     function getDisplayText(): string {
-        const then = _context.parameters.dateTime.raw;
-        const now = new Date();
+        const then = _context.parameters.dateTime.raw
+        const now = new Date()
         if (then === null) {
-            return "---";
+            return '---'
         }
-        return countdown(then, now).toString();
+        return countdown(then, now).toString()
     }
 
     return {
         init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
-            _context = context;
-            _countdownDiv = container.appendChild(document.createElement("div"));
-            start();
+            _context = context
+            _countdownDiv = container.appendChild(document.createElement("div"))
+            start()
         },
         updateView(context: ComponentFramework.Context<IInputs>): void {
-            _context = context;
+            _context = context
         },
         getOutputs(): IOutputs {
-            return {};
+            return {}
         },
         destroy(): void {
             if (_interval === null) {
-                return;
+                return
             }
-            window.clearInterval(_interval);
-            _interval = null;
+            window.clearInterval(_interval)
+            _interval = null
         }
     }
 }
---
+++ setup.bat
@@ -0,0 +1,3 @@
+@echo off
+
+CALL yarn --cwd .\control
