3b9cb95aea4beef4a4a7f8a6bd69f450fc50f662
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-12-15 20:58:54 +0000

    Bump version to 0.3.0

--- control/PCFCountdown/index.ts
+++ control/PCFCountdown/index.ts
@@ -1,49 +1,49 @@
 import { IInputs, IOutputs } from './generated/ManifestTypes'
 import * as countdown from 'countdown'
 
-export function PCFCountdown(): ComponentFramework.StandardControl<IInputs, IOutputs> {
+export class PCFCountdown implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
-    let _context: ComponentFramework.Context<IInputs>
-    let _countdownDiv: HTMLDivElement
-    let _interval: number | null
+    private context: ComponentFramework.Context<IInputs>
+    private countdownDiv: HTMLDivElement
+    private interval: number | null
 
-    function start(): void {
-        _interval = window.setInterval(loop, 1000) // Every second
+
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
+        this.context = context
+        this.countdownDiv = container.appendChild(document.createElement("div"))
+        this.start()
+    }
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        this.context = context
+    }
+    public getOutputs(): IOutputs {
+        return {}
+    }
+    public destroy(): void {
+        if (this.interval === null) {
+            return
+        }
+        window.clearInterval(this.interval)
+        this.interval = null
     }
 
-    function loop(): void {
-        const text = getDisplayText()
-        _countdownDiv.innerText = text
-        _countdownDiv.title = text
+    private start = () => {
+        this.loop()
+        this.interval = window.setInterval(this.loop, 1000) // Every second
     }
 
-    function getDisplayText(): string {
-        const then = _context.parameters.dateTime.raw
+    private loop = () => {
+        const text = this.getDisplayText()
+        this.countdownDiv.innerText = text
+        this.countdownDiv.title = text
+    }
+
+    private getDisplayText = () => {
+        const then = this.context.parameters.dateTime.raw
         const now = new Date()
         if (then === null) {
             return '---'
         }
         return countdown(then, now).toString()
-    }
-
-    return {
-        init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
-            _context = context
-            _countdownDiv = container.appendChild(document.createElement("div"))
-            start()
-        },
-        updateView(context: ComponentFramework.Context<IInputs>): void {
-            _context = context
-        },
-        getOutputs(): IOutputs {
-            return {}
-        },
-        destroy(): void {
-            if (_interval === null) {
-                return
-            }
-            window.clearInterval(_interval)
-            _interval = null
-        }
     }
 }
