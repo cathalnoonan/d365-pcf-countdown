f4cd9fdaa2b15150262ac15e4a5f75701143d59f
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-12-15 20:41:30 +0000

    Build script maintainence

--- clean.bat
+++ clean.bat
@@ -1,9 +1,3 @@
 @echo off
 
-CALL npx rimraf ./control/node_modules
-CALL npx rimraf ./control/obj
-CALL npx rimraf ./control/out
-CALL npx rimraf ./control/PCFCountdown/out
-
-CALL npx rimraf ./solution/bin
-CALL npx rimraf ./solution/obj
+call npx rimraf ./**/node_modules ./**/bin ./**/obj ./**/out ./**/dist
