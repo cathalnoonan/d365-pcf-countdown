0fb8358699abd29d21befab8fe0464ea57c94509
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-06-07 21:26:09 +0100

    Restore/build directly in the workflow instead of using build script

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
 @echo off
 
+cd ./control
+npm install --no-audit --silent
+npm audit --prod
+cd ../
+
 call dotnet build ./solution -c Release
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+npm install --prefix ./control --no-audit --silent
+npm audit --prefix ./control --prod
+
 dotnet build ./solution -c Release
