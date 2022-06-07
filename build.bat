23ae144ec13eab6296dba4829017b4d47d71b2e3
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-06-07 21:49:50 +0100

    Fix windows build script

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 @REM On windows, we can't rely on npm's "--prefix" option
 cd ./control
-npm install --no-audit --ignore-scripts --no-fund
-npm audit --prod
+call npm install --no-audit --ignore-scripts --no-fund
+call npm audit --prod
 cd ../
 
 call dotnet build ./solution -c Release
