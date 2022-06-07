376e56c1ae962ee20f4a211684c3ca76fd38dfdf
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-06-07 21:43:35 +0100

    Remove silent flag in npm scripts

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 
+@REM On windows, we can't rely on npm's "--prefix" option
 cd ./control
-npm install --no-audit --silent
+npm install --no-audit --ignore-scripts --no-fund
 npm audit --prod
 cd ../
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm install --prefix ./control --no-audit --silent
+npm install --prefix ./control --no-audit --ignore-scripts --no-fund
 npm audit --prefix ./control --prod
 
 dotnet build ./solution -c Release
